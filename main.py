d44883d9ec22734bb1b9a18f75d3a689fa603bdf
dallosnoel <71691760+dallosnoel@users.noreply.github.com>
2022-01-21 10:23:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+1. Feladat
+Írj eljárást, amely egy tetszőleges szöveget, ill. alakzatot ír ki a képernyőre!
+'''
+
+def valami(szoveg, alakzat):
+  print(f"A szöveg: {szoveg}\nAz alakzat: {alakzat}")
+
+valami("asd", "OOOO")
